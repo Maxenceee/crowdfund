155f430866cc2b1bf081fad2b3b59586ab861d77
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2021-05-19 14:16:23 +0200

    upload

---
+++ public/javascripts/Script.js
@@ -0,0 +1,268 @@
+const finishButton = document.querySelectorAll('.ms-vl-button')
+const bookmarkButton = document.querySelector('.bookmark-button')
+const closeButton = document.querySelector('.check-cf')
+const indicator = document.querySelectorAll('.ms-indicator')
+const pastilIndicator = document.querySelectorAll('.ms-indicator-inner')
+const pastilContainerView = document.querySelectorAll('.ms-element')
+const vlViewTextField = document.querySelectorAll('.ms-validating')
+const modalView = document.querySelector('.ms-body')
+const rewardButton = document.querySelectorAll('.sub-card-button')
+
+const backerView = document.querySelector('.modal-selector')
+
+const backerButton = document.querySelector('.rsv-button')
+const closeBacker = document.querySelector('.close-button')
+
+const pledgeInput = document.querySelectorAll('.pledge-input')
+
+const msPrice = document.querySelectorAll('.ms-price')
+
+const signBtn = document.querySelector('.sign-btn')
+const signContainer = document.querySelector('.sign-container')
+const closeSignBtn = document.querySelector('.close-sign-btn')
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li')
+
+let pricesList = [5, 25, 75, 200]
+
+
+finishButton.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (document.querySelector('.bg-modal').style.display == '' || document.querySelector('.bg-modal').style.display == 'none') {
+            window.scroll(0, 0)
+            closeBackerWindow()
+            openTanksWindow()
+        }
+    })
+})
+
+backerButton.addEventListener('click', () => {
+    window.scroll(0, 0)
+    if (backerView.style.display == '' || backerView.style.display == 'none') {
+        backerView.style.display = 'flex'
+        setTimeout(() => { 
+            document.body.style.height = "100%"
+            document.body.style.overflow = "hidden";
+        }, 500);
+    }
+})
+
+pastilContainerView.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        if (pastilIndicator[index].classList.contains('toggle-indicator')) {
+            console.log('already toggle')
+            //toggleOff(index)
+        } else {
+            pastilIndicator.forEach((pastil, pindex) => {
+                if (pastil.classList.contains('toggle-indicator')) {
+                    toggleOff(pindex)
+                    msPrice[index].innerHTML = pricesList[index]
+                    pledgeInput[index].classList.remove('input-ph')
+                    pledgeInput[index].placeholder ="Enter your pledge"
+                }
+            })
+            toggleOn(index)
+        }
+    })  
+});
+
+rewardButton.forEach(element => {
+    element.addEventListener('click', () => {
+        window.scroll(0, 0)
+        openTanksWindow()
+    })
+})
+
+closeButton.addEventListener('click', () => {
+    document.querySelector('.bg-modal').style.display = 'none';
+    document.body.style.overflow = "initial";
+})
+
+closeBacker.addEventListener('click', () => {
+    closeBackerWindow()
+    pastilIndicator.forEach((pastil, pindex) => {
+        if (pastil.classList.contains('toggle-indicator')) {
+            toggleOff(pindex)
+            msPrice[index].innerHTML = pricesList[index]
+            pledgeInput[index].classList.remove('input-ph')
+            pledgeInput[index].placeholder ="Enter your pledge"
+        }
+    })
+    document.querySelectorAll('.we-up').style.display = 'none'
+})
+
+bookmarkButton.addEventListener('click', () => {
+    if (bookmarkButton.style.flexDirection == 'row' || bookmarkButton.style.flexDirection == '') {
+        bookmarkButton.style.flexDirection = 'row-reverse'
+        document.querySelector('.bk-txt').style.color = "#137972"
+        document.querySelector('.bk-txt').innerHTML = "Bookmarked";
+        console.log('bookmarked')
+    } else {
+        bookmarkButton.style.flexDirection = 'row'
+        document.querySelector('.bk-txt').style.color = "#717171"
+        document.querySelector('.bk-txt').innerHTML = "Bookmark";
+        console.log('unbookmarked')
+    }
+})
+
+function openTanksWindow() {
+    document.querySelector('.bg-modal').style.display = 'flex'
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+}
+
+function closeBackerWindow() {
+    document.querySelector('.modal-selector').style.display = 'none'
+    document.body.style.overflow = "initial";
+}
+
+function toggleOff(pindex) {
+    pastilIndicator[pindex].classList.toggle('toggle-indicator')
+    pastilContainerView[pindex].classList.toggle('toggle-border')
+    vlViewTextField[pindex].classList.toggle('toggle-indicator')
+    console.log('removed', pindex)
+}
+function toggleOn(index) {
+    pastilIndicator[index].classList.toggle('toggle-indicator')
+    pastilContainerView[index].classList.toggle('toggle-border')
+    vlViewTextField[index].classList.toggle('toggle-indicator')
+    console.log('clicked', index)
+}
+
+function PLInpute() {
+    pledgeInput.forEach((element, index) => {
+        if (element === document.activeElement) {
+            console.log('Element has focus!');
+            if (!isNaN(element.value)) {
+                msPrice[index].innerHTML = element.value
+                if (element.value == "") {
+                    msPrice[index].innerHTML = "0"
+                }
+            }
+        } else {
+            console.log(`Element is not focused.`);
+        }
+    })
+}
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        pledgeInput.forEach((element, index) => {
+            if (element === document.activeElement) {
+                console.log('Element has focus!');
+                if (isNaN(element.value)) {
+                    element.value = ""
+                } else {
+                    checkPriceValue(element.value, index)
+                    element.value = ""
+                }
+            } else {
+                console.log('Element is not focused.');
+            }
+        })
+    }
+});
+
+function checkPriceValue(value, index) {
+    switch (index) {
+        case 0:
+            console.log('current index 1', index)
+            console.log('no limit', value)
+            msPrice[index].innerHTML = value;
+            break;
+        case 1:
+            console.log('current index 2', index)
+            testValue(value, 25, index)
+            break;
+        case 2:
+            console.log('current index 3', index)
+            testValue(value, 75, index)
+            break;
+        case 3:
+            console.log('current index 4', index)
+            testValue(value, 200, index)
+            break;
+        default:
+            break;
+    }
+}
+
+function testValue(value, minValue, index) {
+    if (value >= minValue) {
+        console.log(`upper than ${minValue}`, value)
+        msPrice[index].innerHTML = value;
+        removeInputClass(index)
+    } else {
+        pledgeInput[index].classList.add('input-ph')
+        pledgeInput[index].placeholder = `Must be more than ${minValue}`;
+        msPrice[index].innerHTML = minValue;
+    }
+}
+
+function removeInputClass(index) {
+    pledgeInput[index].classList.remove('input-ph')
+    pledgeInput[index].placeholder ="Enter your pledge"
+}
+
+document.querySelectorAll('.modal-present').forEach(element => {
+    element.addEventListener("click", (evt) => {
+        const flyoutElement = document.getElementById("flyout-example");
+        let targetElement = evt.target;
+    
+        do {
+            if (targetElement == flyoutElement) {
+                console.log("Clicked inside!")
+                return;
+            }
+            // Go up the DOM
+            targetElement = targetElement.parentNode;
+        } while (targetElement);
+        
+        console.log("Clicked outside!")
+        closeBackerWindow()
+    });
+})
+
+function openSign() {
+    signContainer.style.display = 'flex';
+    //document.querySelector('.container').style.animation = 'showAnim 0.5s ease forwards';
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+    if (burger.style.display == 'block') {
+        nav.style.transform = 'translateX(-100%)'
+        burger.classList.toggle('toggle');
+    }
+}
+
+function closeSign() {
+    signContainer.style.display = 'none'
+    document.body.style.overflow = "initial";
+}
+
+closeSignBtn.addEventListener('click', () => {
+    closeSign()
+})
+
+signBtn.addEventListener('click', () => {
+    openSign()
+})
+
+const navSlide = () => {
+    burger.addEventListener('click', () => {
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                nav.style.transform = 'translateX(-100%)'
+                link.style.animation = ``
+            } else {
+                nav.style.transform = 'translateX(0%)'
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
---
+++ public/javascripts/sign.js
@@ -0,0 +1,174 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+const usernames_field = document.querySelector(".Username");
+const password_field = document.querySelector(".Password");
+const email_field = document.querySelector(".Email");
+
+const usernames_field_S = document.querySelector(".UsernameS");
+const password_field_S = document.querySelector(".PasswordS");
+
+const login_btn = document.querySelector("#Login");
+const signup_btn = document.querySelector("#sign-up");
+
+const wrongIndicator = document.querySelectorAll('.we-up')
+
+const inputs = document.querySelectorAll('.SIU-tf')
+
+var usernames = ['maxence'];
+var passwords = ['-1402147925'];
+var emails = ["maxence.gama@gmail.com"];
+
+var isSignUp = false
+
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      console.log("juste appear")
+      container.classList.remove("sign-up-mode");
+    }
+};
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+    isSignUp = true
+    setTimeout(() => { clearTextflied() }, 1000);
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+    isSignUp = false
+    setTimeout(() => { clearTextflied() }, 1000);
+});
+
+function clearTextflied() {
+    usernames_field.value = ''
+    email_field.value = ''
+    password_field.value = ''
+    usernames_field_S.value = ''
+    password_field_S.value = ''
+    wrongIndicator.forEach(e => {
+        e.style.display = 'none'
+    })
+}
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        switch (isSignUp) {
+            case true:
+                signupUser()
+            case false:
+                signinUser()
+        }
+    }
+});
+
+function SIUInpute() {
+    inputs.forEach((element, index) => {
+        if (element === document.activeElement) {
+            console.log('Element has focus!');
+            switch (isSignUp) {
+                case true:
+                    wrongIndicator[1].style.display = 'none'
+                case false:
+                    wrongIndicator[0].style.display = 'none'
+            }
+        } else {
+            console.log('Element is not focused.');
+        }
+    })
+}
+
+login_btn.addEventListener('click', () => {
+    signinUser()
+})
+
+signup_btn.addEventListener('click', () => {
+    console.log("u:", usernames_field_S.value, "p:", password_field_S.value, "e:", email_field.value)
+    signupUser()
+})
+
+function signinUser() {
+    for (var i in usernames) {
+        //console.log("u:", usernames[i], "in:" , usernames_field.value)
+        if (usernames[i] == usernames_field.value) {
+            //console.log("did")
+            if (passwords[i] == password_field.value.hashCode()) {
+                console.log("granted")
+                //window.open("https://google.com", "_blank")
+                closeSign()
+            } else {
+                console.log("password or username incorrect")
+                wrongIndicator[0].style.display = 'flex'
+                console.log(passwords[i], password_field.value.hashCode())
+            }
+        } else {
+            console.log("not found")
+            wrongIndicator[0].style.display = 'flex'
+        }
+    }
+}
+
+function signupUser() {
+    if (usernames_field_S.value != "" && password_field_S.value != "" && email_field.value != "") {
+        for (var i in usernames) {
+            if (usernames[i] == usernames_field_S.value) { //check if user already registered
+                if (passwords[i] == password_field_S.value) {
+                    console.log("already registered");
+                    wrongIndicator[1].innerHTML = 'You are already registered'
+                    wrongIndicator[1].style.display = 'flex'
+                } else {
+                    console.log("username already used")
+                    wrongIndicator[1].innerHTML = 'Username unavailable'
+                    wrongIndicator[1].style.display = 'flex'
+                }
+            } else if (emails[i] == email_field.value) { //check if mail already used
+                console.log("mail already used");
+                wrongIndicator[1].innerHTML = 'This email is used with another account'
+                wrongIndicator[1].style.display = 'flex'
+            } else if (ValidateEmail(email_field.value) == false) {
+                console.log("wrong email");
+                wrongIndicator[1].innerHTML = 'Incorrect email address'
+                wrongIndicator[1].style.display = 'flex'
+            } else {            
+                console.log(ValidateEmail(email_field.value))
+                usernames.push(usernames_field_S.value);
+                passwords.push(password_field_S.value.hashCode());
+                emails.push(email_field.value);
+                console.log(usernames, passwords, emails)
+                closeSign()
+                //window.open("https://google.com", "_blank")
+            }
+        }
+    } else {
+        console.log("missing one at least");
+        wrongIndicator[1].innerHTML = 'You need to fill all the field'
+        wrongIndicator[1].style.display = 'flex'
+    }
+}
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+function closeSign() {
+    document.querySelector('.sign-container').style.display = 'none'
+    document.body.style.overflow = "initial";
+    container.classList.remove("sign-up-mode");
+    clearTextflied()
+}
+
+function ValidateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+        return (true)
+    }
+    return (false)
+}
