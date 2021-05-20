282f0258b666e0bed2194bc1264fc7baad9376ba
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2021-05-20 20:41:52 +0200

    mltiple updates

---
+++ public/javascripts/scroll-trigger.js
@@ -0,0 +1,40 @@
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.style.animation = `pb-entry-animation 3s ease forwards`;
+        //setTimeout(() => { element.classList.add('pb-isvisible'); }, 3000);
+      } else {
+        //element.classList.remove('pb-isvisible');
+      }
+    });
+
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
